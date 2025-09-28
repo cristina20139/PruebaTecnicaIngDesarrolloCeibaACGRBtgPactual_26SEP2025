f7e2bd39e4291cf8231642ca45aaa89d0d29232b
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2025-09-27 21:17:15 -0500

    feat(route): agregar ruta para Suscribirse a Fondo
    Se agrega la ruta:
    - Suscribirse a Fondo (suscribirFondo.tsx)
    
    Implementación siguiendo principios de SOLID y Arquitectura Hexagonal:
    
    - Separación de responsabilidades (Single Responsibility): SuscribirFondoPage se encarga únicamente de la interfaz para suscribirse a un fondo, mientras que los servicios de dominio gestionan la lógica de negocio relacionada con la suscripción.
    
    - Uso de ports y adapters: La llamada al backend se abstrae mediante la función de usecase registrarSuscripcion que actúa como puerto. La implementación concreta en suscripcionesApi.ts funciona como adaptador, separando la infraestructura del dominio.
    
    - Facilita mantenimiento y extensión: Gracias a esta separación, se puede modificar o ampliar la ruta, la UI o la lógica de suscripción sin afectar otras funcionalidades, permitiendo añadir nuevas características sin romper la aplicación existente.

---
+++ REACT/BtgFondosFrontendReactACGR/app/routes/suscribirFondo.tsx
@@ -0,0 +1,6 @@
+// app/routes/suscribirFondo.tsx
+import SuscribirFondoPage from "../../ui/pages/SuscribirFondoPage";
+
+export default function SuscribirFondoRoute() {
+  return <SuscribirFondoPage />;
+}
