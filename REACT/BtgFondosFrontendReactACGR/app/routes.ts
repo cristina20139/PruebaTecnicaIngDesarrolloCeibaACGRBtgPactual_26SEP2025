d93ce51a7305ff6cf105ff98fa2ed8487abecd6e
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2025-09-27 21:37:43 -0500

    feat(routes): agregar ruta para listar Clientes
    Se agrega la ruta:
    - Clientes (clientes.tsx)
    
    Implementación siguiendo principios de SOLID y Arquitectura Hexagonal:
    
    - Separación de responsabilidades (Single Responsibility): La ruta ClientesRoute se encarga únicamente de renderizar la página de clientes (ClientesPage), sin involucrarse en la lógica de negocio.
    
    - Uso de ports y adapters: La ruta conecta la UI (ClientesPage) con los casos de uso (ListarClientes) mediante el adaptador de rutas, separando la navegación de la lógica de dominio.
    
    - Facilita mantenimiento y extensión: Permite modificar la navegación o agregar nuevas rutas de manera modular sin afectar otras partes de la aplicación.

--- REACT/BtgFondosFrontendReactACGR/app/routes.ts
+++ REACT/BtgFondosFrontendReactACGR/app/routes.ts
@@ -5,5 +5,7 @@ export default [
   route("fondos", "routes/fondos.tsx"), // 👈 agrega esta línea
   route("crear-fondo", "routes/crearFondo.tsx"), // 👈 nueva ruta para Crear Fondo
   route("suscribir-fondo", "routes/suscribirFondo.tsx"), // 👈 nueva ruta para Suscribirse a Fondo
+  route("clientes", "routes/clientes.tsx"), // 👈 nueva ruta para listar Clientes
+
 
 ] satisfies RouteConfig;
